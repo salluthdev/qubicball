45740c99f67e86b91c8666114838b8b56e7f78e4
Faisal L <salluthdev@gmail.com>
2022-07-19 15:37:11 +0700

    Perubahan tampilan header saat halaman discroll

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,13 @@
+// Change header on scroll
+window.onscroll = function() {
+  if (window.scrollY >= 20 || window.pageYOffset >= 20) {
+    document.getElementsByTagName('header')[0].classList.add('scroll');
+  } else {
+    document.getElementsByTagName('header')[0].classList.remove('scroll');
+  }
+};
+
+
 // Open sidebar navigation (mobile)
 var sidebarNav = document.querySelector('.sidebar-nav');
 
